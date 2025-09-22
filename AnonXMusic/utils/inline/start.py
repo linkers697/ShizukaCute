2828b49622e292f03bdb65c90e74a8006de0c51f
linkers697 <music3fhhgyhfgh@gmail.com>
2025-09-23 03:26:30 +0530

    Update start.py

--- AnonXMusic/utils/inline/start.py
+++ AnonXMusic/utils/inline/start.py
@@ -32,7 +32,7 @@ def private_panel(_):
         [
             InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT),
         ],
-        [InlineKeyboardButton(text="• sᴏᴜʀᴄᴇ ᴄᴏᴅᴇ •", url=f"https://github.com/xbitcode/music.git")],
+        [InlineKeyboardButton(text="𝗔𝗻𝗼𝗻𝘆", url=f"https://t.me/TryAnony")],
     
     ]
     
